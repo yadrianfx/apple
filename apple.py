44ff3fec47884772fe245e59dc850225ed8b9be7
yadrianfx <79981002+yadrianfx@users.noreply.github.com>
2022-04-08 00:52:57 +0200

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,47 @@
+print("- - - - - - - ")
+print("-   Iphone  - ")
+print("- - - - - - - ")
+print("Remember, if its neccesary ask something, ask with Yes (Y) or No (N)")
+print("If I ask for you mobile phone, don`t tell me with Max or Pro, for example dont tell me Iphone 12 Pro Max, tell me Iphone 12, and the possible answers are from the Iphone 11 up")
+which =input("You like Iphones?: ")
+if which == "N":
+    print("Okey, so I`dont have questions for you")
+    print("See you next time!")
+if which == "Y":
+    print("Now, I`m gonna tell some stats about each in general ")
+battery =input("Some people say that Apple its worst than Android because the battery lasts less, are you agree with that? : ")
+if battery == "N":
+    print("A study was made on the performance of the batteries, based on Iphone 13 and higher. The iPhone 13 Pro Max reaches 28 hours in video playback. The iPhone 13 Pro at 22 hours of battery life, and the iPhone 13 at 19 hours of battery life.")
+if battery == "Y":
+    why = input("Tell us why: ")
+screen = input("Now about the screen, an experiment was done on the streets of London, on the screens of the latest iPhones, most people said that the screen resisted falls well, and that it had an almost perfect size, are you agree with that: ")
+if screen == "N":
+    why2 = input("Tell us why: ")
+if screen == "Y":
+    print("Okey, let's go to the next question")
+volume = input("With the volume, Let's take the Iphone 11, Iphone 11 pro and Iphone 11 Pro Max for reference. A study from the US in the summer of 2021, citizens said that it had quite good volume due to its dB, are you agree with that?: ")
+if volume == "N":
+    why3 = input("Tell us why: ")
+if volume == "Y":
+    print("Okey, let's go to the final question")
+whichiphone = input("Finally, I want to know which Iphone do you have: ")
+if whichiphone == "Iphone 11":
+    print("Is the same like mine, in my opinion is one of the best Iphones from the actual market")
+    opinion = input("Do you have the same opinion like me?: ")
+if whichiphone == "Iphone 12":
+    print("I dont have this mobile phone so I cant give my personal opinion, so you can check the personal opinions for some people that have this mobile phone: https://www.xataka.com/analisis/iphone-12-analisis-caracteristicas-precio-especificaciones")
+if whichiphone == "Iphone 13":
+    print("I dont have this mobile phone so I cant give my personal opinion, so you can check the personal opinions for some people that have this mobile phone: https://www.xataka.com/analisis/iphone-13-analisis-caracteristicas-precio-especificaciones")
+print("This was made by Adrián García 1 BCSºC")
+
+
+
+
+
+
+
+
+
+    
+   
+             
